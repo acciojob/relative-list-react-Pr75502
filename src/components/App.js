3db0932b2fb8016b4bccdd39bffc3ec7c2801ef5
prince prasad <pp1614582@gmail.com>
2025-04-01 19:34:58 +0530

    commit 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const relatives=[]
   return (
+   
     <div id="main">
-               {/* Do not remove the main div */}
+      <ol key="relativeList">
+        {relatives.map((item, index) => (
+          <li key={`relativeListItem${index+1}`}>{item}</li>
+        ))}
+          
+      
+
+      </ol>
+      <button type="button"
+        onClick= {relativeList}
+        
+        
+      ></button>
     </div>
   )
 }
