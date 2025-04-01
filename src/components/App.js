3e5d1f50c772a34b18eb036a053ae7a16778df51
prince prasad <pp1614582@gmail.com>
2025-04-01 21:09:40 +0530

    hello 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
 const App = () => {
-  const relatives=[]
+  const relatives = ["Alice", "Bob", "Charlie"]
   return (
    
     <div id="main">
       <ol key="relativeList">
         {relatives.map((item, index) => (
-          <li key={`relativeListItem${index+1}`}>{item}</li>
+          <li key={`relativeListItem${index + 1}`}>{item}</li>
         ))}
+
           
       
 
